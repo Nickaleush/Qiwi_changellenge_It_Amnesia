988bfc223aba0f433ef01855a30fa5a655e2ffa6
Nickaleush <nickaleush@mail.ru>
2022-11-12 23:24:15 +0300

    Qiwi-track UserContacts intent Implementation + roundedImageView

--- app/src/main/java/com/example/qiwi_changellenge_it_amnesia/ui/authentication/auth/AuthFragment.kt
+++ app/src/main/java/com/example/qiwi_changellenge_it_amnesia/ui/authentication/auth/AuthFragment.kt
@@ -11,6 +11,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
+import androidx.navigation.fragment.findNavController
 import com.example.qiwi_changellenge_it_Amnesia.R
 import com.example.qiwi_changellenge_it_amnesia.App
 import com.example.qiwi_changellenge_it_amnesia.domain.models.Code
@@ -22,6 +23,8 @@ import com.example.qiwi_changellenge_it_amnesia.ui.qr.QRFragment
 import com.example.qiwi_changellenge_it_amnesia.utils.countryPicker.CCPicker
 import com.example.qiwi_changellenge_it_amnesia.utils.countryPicker.adapter.CountryPickerAdapter
 import com.example.qiwi_changellenge_it_amnesia.utils.countryPicker.model.Country
+import com.example.qiwi_changellenge_it_amnesia.utils.countryPicker.utils.fadeIn
+import com.example.qiwi_changellenge_it_amnesia.utils.countryPicker.utils.fadeOut
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.jakewharton.rxbinding.widget.RxTextView
@@ -32,12 +35,25 @@ import rx.android.schedulers.AndroidSchedulers
 import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+import android.provider.ContactsContract
+
+import android.content.Intent
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.database.Cursor
+import android.net.Uri
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+import android.provider.ContactsContract.CommonDataKinds.Phone
 
 class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
 
     @Inject
     lateinit var sharedPreferences: SharedPreferences
 
+    private val REQUEST_CODE = 1
 
     private lateinit var sheetView: View
 
@@ -67,6 +83,13 @@ class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
                     countryCodeTextView.setText(countryCode)
                 }
             })
+        }
+
+        numberFromContactsImageView.setOnClickListener {
+            val uri = Uri.parse("content://contacts")
+            val intent = Intent(Intent.ACTION_PICK, uri)
+            intent.type = Phone.CONTENT_TYPE
+            startActivityForResult(intent, REQUEST_CODE)
         }
 
         textViewRegister.setOnClickListener {
@@ -116,31 +139,9 @@ class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
     override fun showError(message: String?): Unit = Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
 
     override fun navToUserProfileFragment() {
-        mBottomSheetDialog.dismissWithAnimation
-    }
-
-    private fun View.fadeIn(duration: Long): Completable {
-        val animationSubject = CompletableSubject.create()
-        return animationSubject.doOnSubscribe {
-            ViewCompat.animate(this)
-                .setDuration(duration)
-                .alpha(1f)
-                .withEndAction {
-                    animationSubject.onComplete()
-                }
-        }
-    }
-
-    private fun View.fadeOut(duration: Long): Completable {
-        val animationSubject = CompletableSubject.create()
-        return animationSubject.doOnSubscribe {
-            ViewCompat.animate(this)
-                .setDuration(duration)
-                .alpha(0f)
-                .withEndAction {
-                    animationSubject.onComplete()
-                }
-        }
+        mBottomSheetDialog.dismiss()
+        Toast.makeText(requireContext(), getString(R.string.AuthenticationSuccess), Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_authFragment_to_QRFragment)
     }
 
     private fun changeConstraintsLogin(set: ConstraintSet) {
@@ -149,8 +150,58 @@ class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
         set.clear(R.id.textViewReadOffer, ConstraintSet.RIGHT)
         set.connect(R.id.textViewReadOffer, ConstraintSet.TOP, R.id.passwordEditText, ConstraintSet.BOTTOM, 36)
         buttonDone.setOnClickListener {
-            presenter.loginWithPhone(UserToLogin(countryCodeTextView.text.toString(),(countryCodeTextView.toString()+phoneEditText)))
+            val phoneNumber = phoneEditText.text.toString().replace(" ", "")
+            val finalPhoneNumber = phoneNumber.replace("-", "")
+            presenter.loginWithPhone(UserToLogin(passwordEditText.text.toString(),(countryCodeTextView.text.toString()+finalPhoneNumber)))
+        }
+    }
 
+    private fun changeConstraintsSignUp(set: ConstraintSet) {
+        set.clear(R.id.textViewReadOffer, ConstraintSet.TOP)
+        set.clear(R.id.passwordEditText, ConstraintSet.TOP)
+        set.connect(R.id.passwordEditText, ConstraintSet.TOP, R.id.editTextName, ConstraintSet.BOTTOM, 36)
+        set.connect(R.id.textViewReadOffer, ConstraintSet.TOP, R.id.passwordEditText, ConstraintSet.BOTTOM, 36)
+
+        buttonDone.setOnClickListener {
+            val phoneNumber = phoneEditText.text.toString().replace(" ", "")
+            val finalPhoneNumber = phoneNumber.replace("-", "")
+            presenter.signUpWithPhone(UserToSignUp(editTextName.text.toString(), passwordEditText.text.toString(),(countryCodeTextView.text.toString()+finalPhoneNumber)))
+        }
+
+        textViewRegister.text = getString(R.string.Login)
+        textViewRegister.setOnClickListener {
+            textViewRegister.text = getString(R.string.NotRegisteredYet)
+            val changeConstraintsLogin = ConstraintSet()
+            changeConstraintsLogin.clone(auth_fragment_layout)
+            changeConstraintsBackToLogin(changeConstraintsLogin)
+            TransitionManager.beginDelayedTransition(auth_fragment_layout)
+            changeConstraintsLogin.applyTo(auth_fragment_layout)
+            editTextName.visibility = View.GONE
+            editTextName.fadeOut(400).subscribe()
+        }
+    }
+
+    private fun changeConstraintsBackToLogin(set: ConstraintSet) {
+        set.clear(R.id.passwordEditText, ConstraintSet.TOP)
+        set.connect(R.id.passwordEditText, ConstraintSet.TOP, R.id.phoneEditText, ConstraintSet.BOTTOM, 36)
+        set.connect(R.id.textViewReadOffer, ConstraintSet.TOP, R.id.passwordEditText, ConstraintSet.BOTTOM, 36)
+
+        buttonDone.setOnClickListener {
+            val phoneNumber = phoneEditText.text.toString().replace("-","")
+            val finalPhoneNumber = phoneNumber.replace(" ", "")
+            presenter.loginWithPhone(UserToLogin(passwordEditText.text.toString(),(countryCodeTextView.text.toString()+finalPhoneNumber)))
+        }
+
+        textViewRegister.setOnClickListener {
+
+            editTextName.visibility = View.VISIBLE
+            passwordEditText.visibility = View.VISIBLE
+            val constraintSetSignUpForm = ConstraintSet()
+            constraintSetSignUpForm.clone(auth_fragment_layout)
+            changeConstraintsSignUp(constraintSetSignUpForm)
+            TransitionManager.beginDelayedTransition(auth_fragment_layout)
+            constraintSetSignUpForm.applyTo(auth_fragment_layout)
+            passwordEditText.fadeIn( 400).mergeWith(editTextName.fadeIn( 400)).subscribe()
         }
     }
 
@@ -179,6 +230,7 @@ class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
     }
 
     private  fun startTime(time: Int){
+
         val tvWrongCodeError  = sheetView.findViewById<TextView>(R.id.tv_wrongCodeError)
         val tvRepeatSendCode  = sheetView.findViewById<TextView>(R.id.textViewRepeatSendCode)
         val tvResendCode  = sheetView.findViewById<TextView>(R.id.tv_resendCode)
@@ -203,6 +255,7 @@ class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
         }
     }
 
+
     private fun updateTimeText(time: Int){
         val tvRepeatSendCode  = sheetView.findViewById<TextView>(R.id.textViewRepeatSendCode)
         requireActivity().runOnUiThread { tvRepeatSendCode?.text = String.format("%s %d:%02d", getString(R.string.resend_two_min), time / 60, time % 60)}
@@ -221,10 +274,10 @@ class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
 
     private fun showResendAction() {
         requireActivity().runOnUiThread {
-            val etTextConfirmCode  = sheetView?.findViewById<EditText>(R.id.editTextConfirmCode)
-            val tvWrongCodeError  = sheetView?.findViewById<TextView>(R.id.tv_wrongCodeError)
-            val tvRepeatSendCode  = sheetView?.findViewById<TextView>(R.id.textViewRepeatSendCode)
-            val tvResendCode  = sheetView?.findViewById<TextView>(R.id.tv_resendCode)
+            val etTextConfirmCode  = sheetView.findViewById<EditText>(R.id.editTextConfirmCode)
+            val tvWrongCodeError  = sheetView.findViewById<TextView>(R.id.tv_wrongCodeError)
+            val tvRepeatSendCode  = sheetView.findViewById<TextView>(R.id.textViewRepeatSendCode)
+            val tvResendCode  = sheetView.findViewById<TextView>(R.id.tv_resendCode)
             etTextConfirmCode?.visibility = View.VISIBLE
             tvWrongCodeError?.visibility = View.GONE
             tvRepeatSendCode?.visibility = View.GONE
@@ -232,48 +285,21 @@ class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
         }
     }
 
-    private fun changeConstraintsSignUp(set: ConstraintSet) {
-        set.clear(R.id.textViewReadOffer, ConstraintSet.TOP)
-        set.clear(R.id.passwordEditText, ConstraintSet.TOP)
-        set.connect(R.id.passwordEditText, ConstraintSet.TOP, R.id.editTextName, ConstraintSet.BOTTOM, 36)
-        set.connect(R.id.textViewReadOffer, ConstraintSet.TOP, R.id.passwordEditText, ConstraintSet.BOTTOM, 36)
-
-        buttonDone.setOnClickListener {
-            presenter.signUpWithPhone(UserToSignUp(editTextName.text.toString(), passwordEditText.text.toString(),(countryCodeTextView.text.toString()+phoneEditText.text.toString())))
-        }
-
-        textViewRegister.text = getString(R.string.Login)
-        textViewRegister.setOnClickListener {
-            textViewRegister.text = getString(R.string.NotRegisteredYet)
-            val changeConstraintsLogin = ConstraintSet()
-            changeConstraintsLogin.clone(auth_fragment_layout)
-            changeConstraintsBackToLogin(changeConstraintsLogin)
-            TransitionManager.beginDelayedTransition(auth_fragment_layout)
-            changeConstraintsLogin.applyTo(auth_fragment_layout)
-            editTextName.visibility = View.GONE
-            editTextName.fadeOut(400).subscribe()
-        }
-    }
-
-    private fun changeConstraintsBackToLogin(set: ConstraintSet) {
-        set.clear(R.id.passwordEditText, ConstraintSet.TOP)
-        set.connect(R.id.passwordEditText, ConstraintSet.TOP, R.id.phoneEditText, ConstraintSet.BOTTOM, 36)
-        set.connect(R.id.textViewReadOffer, ConstraintSet.TOP, R.id.passwordEditText, ConstraintSet.BOTTOM, 36)
-
-        buttonDone.setOnClickListener {
-            presenter.loginWithPhone(UserToLogin(countryCodeTextView.text.toString(),(countryCodeTextView.toString()+phoneEditText)))
-        }
-
-        textViewRegister.setOnClickListener {
-
-            editTextName.visibility = View.VISIBLE
-            passwordEditText.visibility = View.VISIBLE
-            val constraintSetSignUpForm = ConstraintSet()
-            constraintSetSignUpForm.clone(auth_fragment_layout)
-            changeConstraintsSignUp(constraintSetSignUpForm)
-            TransitionManager.beginDelayedTransition(auth_fragment_layout)
-            constraintSetSignUpForm.applyTo(auth_fragment_layout)
-            passwordEditText.fadeIn( 400).mergeWith(editTextName.fadeIn( 400)).subscribe()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                val uri: Uri = data?.data!!
+                val projection = arrayOf(Phone.NUMBER, Phone.DISPLAY_NAME)
+                val cursor: Cursor? = requireContext().contentResolver.query(uri, projection, null, null, null)
+                cursor?.moveToFirst()
+                val numberColumnIndex: Int? = cursor?.getColumnIndex(Phone.NUMBER)
+                val number: String? = numberColumnIndex?.let {cursor.getString(it)}
+                pickedPhoneNumber = if (number?.take(1).equals("+")) number?.takeLast(13).toString()
+                else number?.takeLast(10).toString()
+                phoneEditText.setText(pickedPhoneNumber)
+                val nameColumnIndex: Int? = cursor?.getColumnIndex(Phone.DISPLAY_NAME)
+            }
         }
     }
 
@@ -281,5 +307,6 @@ class AuthFragment :  BaseFragment<AuthPresenterImpl>(), AuthView {
         private var remainSeconds = 120
         private var timerConfirmAccount: Timer? = null
         var confirmCodeAccount = ""
+        var pickedPhoneNumber =""
     }
 }
